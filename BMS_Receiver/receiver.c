e0ef683f9a641b7d032190d54c26e9ecc78ffff1
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-10 13:05:40 +0530

    Update receiver.c

--- BMS_Receiver/receiver.c
+++ BMS_Receiver/receiver.c
@@ -22,3 +22,73 @@ void ReadBMSParametersfromConsole(float* Temp,float* SOC , int TotalSamples)
 	  printf("%.3f \t %.3f\n",Temp[i],SOC[i]);
     }
 }
+
+float CalculateMinValue(float* InputData, int TotalSamples)
+{
+  float MinValue = InputData[0];
+    for(int i=1; i<TotalSamples; i++)
+    {
+        if(MinValue > InputData[i])
+        {
+            MinValue = InputData[i];
+        }
+    }
+    return MinValue;
+}
+
+
+
+float CalculateMaxValue(float* InputData, int TotalSamples)
+{
+  float MaxValue = InputData[0];
+    for(int i=1; i<TotalSamples; i++)
+    {
+        if(MaxValue < InputData[i])
+        {
+            MaxValue = InputData[i];
+        }
+    }
+    return MaxValue;
+}
+                        
+
+float CalculateAvg(float* InputData, int TotalSamples)
+{
+    float Avg = 0;
+    float Sum = 0;
+    for(int i = 0; i < TotalSamples; i++)
+    {
+        Sum = Sum + InputData[i];
+    }
+	    Avg = Sum/TotalSamples;
+    return Avg;
+}
+
+			
+void CalculateSMA(float *InputData, int windowsize, float *MovingAvg)
+{
+     float Sum = 0;
+     for (int i = 0; i < 50; i++) 
+     {
+         Sum = Sum + InputData[i];
+         if (i >= windowsize)
+	 {
+	     Sum -= InputData[i-windowsize];
+	 }
+	 if(i >= windowsize - 1)
+	 {
+            MovingAvg[i-windowsize] = Sum/windowsize;
+	 }
+     }
+}
+			
+	
+void PrintMinMaxValues(float* Temp, float* SOC, int TotalSamples) 
+{
+  float mintemp, maxtemp, minSOC, maxSOC;
+  mintemp = CalculateMinValue(Temp, TotalSamples);
+  maxtemp = CalculateMaxValue(Temp, TotalSamples);
+  minSOC = CalculateMinValue(SOC, TotalSamples);
+  maxSOC = CalculateMaxValue(SOC, TotalSamples);
+  printf("min current: %f max current: %f min voltage: %f max voltage: %f\n", mintemp, maxtemp, minSOC, maxSOC);
+}
