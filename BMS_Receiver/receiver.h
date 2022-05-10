2b596df50b73b8d73177730510ea3d6e6f5de4ea
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-10 16:41:43 +0530

    Update receiver.h

--- BMS_Receiver/receiver.h
+++ BMS_Receiver/receiver.h
@@ -1,6 +1,6 @@
 void ReadInputfromConsole(float* Temp,float* Voltage , int SampleCount);
-float FindMininimumSample(float* InputData , int SampleCount);
-float FindMaximumSample(float* InputData , int SampleCount);                       
-float FindSampleAverage(float* InputData, int TotalSamples);                       
+float FindMininimumSample(float* BMSParameterInput , int SampleCount);
+float FindMaximumSample(float* BMSParameterInput , int SampleCount);                       
+float FindSampleAverage(float* BMSParameterInput, int TotalSamples);                       
 void CalculateSMA(float *InputData, int windowsize, float *movingAvg);                   
 void PrintMinMaxValues(float* Temp, float* SOC, int TotalSamples) ;
